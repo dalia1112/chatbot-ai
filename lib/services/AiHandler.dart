646e0dc4d31a96506bdc22d7cd07d7aefbc437b6
dalia elsayed <daliaelsayed1112@gmail.com>
2024-04-02 19:46:10 +0200

    first commit

--- lib/services/AiHandler.dart
+++ lib/services/AiHandler.dart
@@ -1,7 +1,7 @@
 import 'package:chat_gpt_sdk/chat_gpt_sdk.dart';
 
 class AIHandler {
-  final _openAI = OpenAI.instance.build(git
+  final _openAI = OpenAI.instance.build(
     token: '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',
     baseOption: HttpSetup(
       receiveTimeout: const Duration(seconds: 60),
